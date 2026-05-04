6dcd79b36c81b1053b80ac26f2c3b5c83b0c5260
ShahriarAlShihab <shahriarshihab96@gmail.com>
2026-05-04 23:43:08 +0600

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
     ],
   },
