5b807a4ac189a94e1bfea024e0bc9e8dea0a5c62
ShahriarAlShihab <shahriarshihab96@gmail.com>
2026-05-05 06:35:17 +0600

    error fixed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-const config = {
+/* eslint-disable import/no-anonymous-default-export */
+export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
-
-export default config;
